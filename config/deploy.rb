8c51e28b8ef81955f9b18d7b2a63632bf971c44d
Tomwey <tomwey@163.com>
2013-09-04 22:49:04 +0800

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+require "bundler/capistrano"
+
+server "112.124.48.51", :web, :app, :db, primary: true
+
+set :application, "faxin"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository,  "git@github.com:tomwey/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+# 保留5个最新的版本
+after "deploy", "deploy:cleanup"
+after "deploy:cleanup", "deploy:remote_rake"
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+  
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml.example"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+  
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+  
+  task :remote_rake, roles: :db do
+    run "cd #{deploy_to}/current; RAILS_ENV=production bundle exec rake db:migrate"
+  end
+  # desc "Make sure local git is in sync with remote."
+  # task :check_revision, roles: :web do
+  #   unless `git rev-parse HEAD` == `git rev-parse origin/master` do
+  #     puts "WARNING: HEAD is not the same as origin/master"
+  #     puts "Run `git push` to sync changes."
+  #     exit
+  #   end
+  # end
+  # before "deploy", "deploy:check_revision"
+end
+
+# namespace :remote_rake do
+#   task :invoke do
+#     run "cd #{deploy_to}/current; RAILS_ENV=production bundle exec rake db:migrate"
+#   end
+# end
+# 
+# namespace :my_tasks do
+#   task :copy_to_public do
+#     run "cp /home/#{user}/share_icon.png #{deploy_to}/current/public/share_icon.png"
+#     run "cp -r /home/#{user}/share_link/ #{deploy_to}/current/public"
+#   end
+# end
