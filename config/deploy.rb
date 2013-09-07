82eb905bfeadd4bc741a19ee38003b6a8590e399
Tomwey <tomwey@163.com>
2013-09-07 15:21:04 +0800

    xxx

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,33 @@ namespace :deploy do
   # before "deploy", "deploy:check_revision"
 end
 
+# 全文检索
+namespace :deploy do
+  task :setup_solr_data_dir do
+    run "mkdir -p #{shared_path}/solr/data"
+  end
+end
+ 
+namespace :solr do
+  desc "start solr"
+  task :start, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  end
+  desc "stop solr"
+  task :stop, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  end
+  desc "reindex the whole database"
+  task :reindex, :roles => :app do
+    stop
+    run "rm -rf #{shared_path}/solr/data"
+    start
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex"
+  end
+end
+ 
+after 'deploy:setup', 'deploy:setup_solr_data_dir'
+
 # namespace :remote_rake do
 #   task :invoke do
 #     run "cd #{deploy_to}/current; RAILS_ENV=production bundle exec rake db:migrate"
