174d1a54567b286d8fdcfca8e437f823d7dc12cd
Tomwey <tomwey@163.com>
2013-11-10 21:02:05 +0800

    xxxxx

--- app/controllers/law_types_controller.rb
+++ app/controllers/law_types_controller.rb
@@ -6,7 +6,7 @@ class LawTypesController < ApplicationController
   def show
     @law_type = LawType.find(params[:id])
     @law_type_name = @law_type.name
-    @laws = @law_type.laws.unscoped.order('pub_date desc').paginate :page => params[:page], :per_page => 30
+    @laws = @law_type.laws.order('pub_date desc').paginate :page => params[:page], :per_page => 30
   end
   
   def new
